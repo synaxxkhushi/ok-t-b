0db776e78fa7ada56c74046733b25b5c75415883
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-04-17 14:22:39 -0700

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ CHAT1       = "@synaxnetwork" # only username supported
 CHAT2       = "@synaxbots" # only username supported
 
 ## TERABOX COOKIES
-COOKIES     = "csrfToken=BnQx-ffuJKamkWLrsx3Ksds4; browserid=qEGrcJcpudjTVSp34yywiOK8KGczvjGYTPI6jJMydd7dpoQVUXCzDlp7JFM=; lang=en; bid_n=18db31bdad1e0ee8094207; stripe_mid=a300821a-4b38-4e0a-a227-5c27f1b7c54789f06e; __stripe_sid=f7b244b5-18a8-4540-bfa0-222b4acc0bf5eb56ba; ndus=Yb47bKTteHuiwDuAJYscQHtlthgbg2-2R4yRRWFe; ndut_fmt=17DAF2FD0DC6DF723D10EB645D49FD8581EFDCCB4A2823D11359EA0CCE270FBE
-" (don't use this)
+COOKIES     = "csrfToken=BnQx-ffuJKamkWLrsx3Ksds4; browserid=qEGrcJcpudjTVSp34yywiOK8KGczvjGYTPI6jJMydd7dpoQVUXCzDlp7JFM=; lang=en; bid_n=18db31bdad1e0ee8094207; stripe_mid=a300821a-4b38-4e0a-a227-5c27f1b7c54789f06e; __stripe_sid=f7b244b5-18a8-4540-bfa0-222b4acc0bf5eb56ba; ndus=Yb47bKTteHuiwDuAJYscQHtlthgbg2-2R4yRRWFe; ndut_fmt=17DAF2FD0DC6DF723D10EB645D49FD8581EFDCCB4A2823D11359EA0CCE270FBE" (don't use this)
 
 # REDIS DATABASE [https://redis.com/try-free]
 HOST        = "redis-16969.c274.us-east-1-3.ec2.cloud.redislabs.com"
