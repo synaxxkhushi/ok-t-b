40b0c0da053de4f2d78632e6937de56fa733d784
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-05-02 04:58:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ CHAT1       = "@synaxnetwork" # only username supported
 CHAT2       = "@synaxbots" # only username supported
 
 ## TERABOX COOKIES
-COOKIES     = "PANWEB=1; csrfToken=jtTLiTvGBxl_IFTle3hwgd06; browserid=oCpAO0XIcEXS61MAHq_S0gea3v-MrA7ZbuYgHcSypkPMjODJI2Yfnbm91DI=; lang=en; TSID=yEwVKMH3QRfeVasoZ2rLwqUDae3tGNdU; __bid_n=18dcc331eb3d5363504207; _ga=GA1.1.2132560438.1708527920; __stripe_mid=bce13f2e-8150-4080-b99f-c602ab9289d26acc64; ndus=Y-R0se7teHui-W2HBiWYGD11Y6j6V_cUIkDTmbRI; _ga_06ZNKL8C2E=GS1.1.1709701123.4.0.1709702212.52.0.0; ndut_fmt=A533422E05B3AA6868B57C3F11A827DD07A068D233C9AC3D2D6907B2472401D4" # get from chrome dev tools ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+COOKIES     = "PANWEB=1; csrfToken=MkqT6gDAocZzpanwxC_qcIQw; browserid=fUSzfZnsfR1v8T0y_C10IUL-DWeJOtfbk54grUAVM47hH5lKtVPR94kTgl0=; lang=en; bid_n=18f391807aeb6405dd4207; stripe_mid=e48c3be4-aa47-471b-8947-da14cd3da49696ddd0; __stripe_sid=26866037-610b-4451-892b-1b83e5e2883739099e; ndus=Y4ZW53CteHuiZEVNT9iX5E4DAdRsCk4v4Inzu5-3; ndut_fmt=B017DAC69BBE8BF18A90900727665A6D45F0C4233898CD62183F562720F53CF3" # get from chrome dev tools ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
 # REDIS DATABASE [https://redis.com/try-free]
 HOST        = "redis-16969.c274.us-east-1-3.ec2.cloud.redislabs.com"
