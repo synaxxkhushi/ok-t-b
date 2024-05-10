16ba182e85dd98cb56028929a5840088e7249b28
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-05-10 16:11:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ CHAT2       = "@synaxbots" # only username supported
 COOKIES     = "PANWEB=1; csrfToken=MkqT6gDAocZzpanwxC_qcIQw; browserid=fUSzfZnsfR1v8T0y_C10IUL-DWeJOtfbk54grUAVM47hH5lKtVPR94kTgl0=; lang=en; bid_n=18f391807aeb6405dd4207; stripe_mid=e48c3be4-aa47-471b-8947-da14cd3da49696ddd0; __stripe_sid=26866037-610b-4451-892b-1b83e5e2883739099e; ndus=Y4ZW53CteHuiZEVNT9iX5E4DAdRsCk4v4Inzu5-3; ndut_fmt=B017DAC69BBE8BF18A90900727665A6D45F0C4233898CD62183F562720F53CF3" # get from chrome dev tools ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
 # REDIS DATABASE [https://redis.com/try-free]
-HOST        = "redis-16969.c274.us-east-1-3.ec2.cloud.redislabs.com"
-PORT        = 16969
-PASSWORD    = "4c2EP43VcEWklp9DpI6Zv5kfPJfecH4w"
+HOST        = "redis-10693.c278.us-east-1-4.ec2.redns.redis-cloud.com"
+PORT        = 10693
+PASSWORD    = "aLYDdunvCU0I0wY3Jrq6s3ehqabHl3Qf"
 
 
 # ---------- DON'T TOUCH THIS ---------- #
